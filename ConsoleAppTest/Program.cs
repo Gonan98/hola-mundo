6ec8879042c074bbdce0e5bd7c88b94c1e60baf1
Gonan98 <andreg-16@hotmail.com>
2019-04-16 18:58:47 -0500

    Se añadio una simple suma

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -11,6 +11,12 @@ namespace ConsoleAppTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo");
+            int a, b, suma;
+            Console.WriteLine("Ingrese 2 numeros: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            suma = a + b;
+            Console.WriteLine("{0} + {1} = {2}", a, b, suma);
             Console.ReadKey();
         }
     }
